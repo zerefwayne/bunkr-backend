e466ddab82e3301b7960c1988498fdcaa66c9fec
Aayush Joglekar <aayushjog@gmail.com>
2020-05-28 01:57:43 +0530

    initalises user repository and usecase

--- user/usecase.go
+++ user/usecase.go
@@ -13,3 +13,28 @@ type Usecase interface {
 	CreateUser(ctx context.Context, user *models.User) error
 	GetAllUsers(ctx context.Context) ([]*models.User, error)
 }
+
+type userUsecase struct {
+	userRepo Repository
+}
+
+// NewUserUsecase ...
+func NewUserUsecase(r Repository) Usecase {
+	return &userUsecase{
+		userRepo: r,
+	}
+}
+
+func (u *userUsecase) GetByID(ctx context.Context, id string) (*models.User, error) {
+	return nil, nil
+}
+
+func (u *userUsecase) GetByUsername(ctx context.Context, username string) (*models.User, error) {
+	return nil, nil
+}
+func (u *userUsecase) CreateUser(ctx context.Context, user *models.User) error {
+	return nil
+}
+func (u *userUsecase) GetAllUsers(ctx context.Context) ([]*models.User, error) {
+	return nil, nil
+}
