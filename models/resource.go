07b32a4fd4d9ebbeec376c31b46a4653cf5c6972
Aayush Joglekar <aayushjog@gmail.com>
2020-05-27 23:36:44 +0530

    Adds User and Resource model

---
+++ models/resource.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+// Resource Defines the Resource Schema
+type Resource struct {
+	ID         string    `json:"id", son:"id"`
+	Content    string    `json:"content", bson:"content"`
+	CreatedAt  time.Time `json:"created_at", bson:"created_at"`
+	CreatedBy  string    `json:"created_by", bson:"created_by"`
+	IsApproved bool      `json:"is_approved", bson:"is_approved"`
+}
---
+++ models/user.go
@@ -0,0 +1,10 @@
+package models
+
+// User Defines the user model
+type User struct {
+	ID       string `json:"id", bson:"_id, omitempty"`
+	Username string `json:"username", bson:"username"`
+	Name     string `json:"name", bson:"name"`
+	Password string `json:"password", bson:"password"`
+	Email    string `json:"email", bson:"email"`
+}
