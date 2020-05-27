b39169fad2bae1cd04362e69d25180584cd1e931
Aayush Joglekar <aayushjog@gmail.com>
2020-05-28 00:43:23 +0530

    Refactors mongodb config to its own file

--- config/config.go
+++ config/config.go
@@ -1,12 +1,7 @@
 package config
 
 import (
-	"context"
-	"fmt"
-	"log"
-
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Config defines the global config object
@@ -26,41 +21,4 @@ func Init() (*Config, error) {
 
 	return newConfig, nil
 
-}
-
-// ConnectMongo connects mongo
-func (c *Config) ConnectMongo() {
-
-	ctx := context.Background()
-
-	host := c.Env.MongoDBEnv.Host
-	port := c.Env.MongoDBEnv.Port
-	database := c.Env.MongoDBEnv.Database
-	user := c.Env.MongoDBEnv.User
-	password := c.Env.MongoDBEnv.Password
-	srvMode := c.Env.MongoDBEnv.SRV
-
-	mongoURI := ""
-
-	if srvMode == "true" {
-		mongoURI = fmt.Sprintf("mongodb+srv://%s:%s@%s", user, password, host)
-	} else {
-		mongoURI = fmt.Sprintf("mongodb://%s:%s/%s", host, port, database)
-	}
-
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		mongoURI,
-	))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	c.MongoDB = client
-
-	if err := c.MongoDB.Ping(context.Background(), nil); err != nil {
-		log.Fatalln(err)
-	} else {
-		log.Printf("database	| connected successfully: %s\n", mongoURI)
-	}
 }
---
+++ config/mongodb.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// ConnectMongo connects mongo
+func (c *Config) ConnectMongo() {
+
+	ctx := context.Background()
+
+	host := c.Env.MongoDBEnv.Host
+	port := c.Env.MongoDBEnv.Port
+	database := c.Env.MongoDBEnv.Database
+	user := c.Env.MongoDBEnv.User
+	password := c.Env.MongoDBEnv.Password
+	srvMode := c.Env.MongoDBEnv.SRV
+
+	mongoURI := ""
+
+	if srvMode == "true" {
+		mongoURI = fmt.Sprintf("mongodb+srv://%s:%s@%s", user, password, host)
+	} else {
+		mongoURI = fmt.Sprintf("mongodb://%s:%s/%s", host, port, database)
+	}
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+		mongoURI,
+	))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.MongoDB = client
+
+	if err := c.MongoDB.Ping(context.Background(), nil); err != nil {
+		log.Fatalln(err)
+	} else {
+		log.Printf("database	| connected successfully: %s\n", mongoURI)
+	}
+}
