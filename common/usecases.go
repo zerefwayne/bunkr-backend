d4a974575eb68cad72ebbc173fc4bdd470e66fe2
Aayush Joglekar <aayushjog@gmail.com>
2020-06-11 23:51:05 +0530

    add comments in common

--- common/usecases.go
+++ common/usecases.go
@@ -5,6 +5,8 @@ import (
 	"github.com/zerefwayne/college-portal-backend/resource"
 )
 
+// Common consists of a usecase instances which can be used throughout the project without
+// causing the problem of cyclic imports
 var (
 	Course   course.Usecase
 	Resource resource.Usecase
